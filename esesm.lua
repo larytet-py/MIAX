acae16359a8dc1d7a4fc5b09948cd65ce48db5e7
larytet <>
2024-11-15 13:45:35 +0200

    update

--- esesm.lua
+++ esesm.lua
@@ -3,11 +3,17 @@ local ESesM = Proto("ESesM", "MIAX ESesM Protocol")
 local f = ESesM.fields
 f.packet_type = ProtoField.string("ESesM.packet_type", "Packet Type", base.ASCII)
 
+
+local e_unexpected_packet_type = ProtoExpert.new("ESesM.unexpected_packet_type.expert", "Unexpected packet type", expert.group.UNDECODED, expert.severity.ERROR)
+local e_packet_too_short = ProtoExpert.new("ESesM.packet_too_short.expert", "Packet is too short", expert.group.MALFORMED, expert.severity.ERROR)
+ESesM.experts = { e_unexpected_packet_type, e_packet_too_short }
+
+
 -- Function to check and add the packet type
 local function check_and_add_packet_type(buffer, subtree, fields)
     local packet_type = buffer(0,2):string()
     if packet_type ~= "N1" then
-        subtree:add_proto_expert_info(PI_UNDECODED, PI_ERROR, "Unexpected packet type: " .. packet_type)
+        subtree:add_proto_expert_info(e_unexpected_packet_type, "Unexpected packet type: " .. packet_type)
         subtree:add(fields.packet_type, buffer(0, 2))
         return false -- Indicate error
     end
@@ -26,7 +32,7 @@ function ESesM.dissector(buffer, pinfo, tree)
 
     -- Ensure there's enough data
     if buffer:len() < 2 then
-        subtree:add_proto_expert_info(PI_MALFORMED, PI_ERROR, "Packet is too short")
+        subtree:add_proto_expert_info(e_packet_too_short, "Packet is too short")
         return
     end
 
